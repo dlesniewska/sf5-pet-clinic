7caead315e4248671be2416a7d458362f110e94e
dlesniewska <32817937+dlesniewska@users.noreply.github.com>
2018-10-29 22:26:25 +0100

    Initial project structure

---
+++ src/main/java/dagimon/spring5course/sf5petclinic/Sf5PetClinicApplication.java
@@ -0,0 +1,12 @@
+package dagimon.spring5course.sf5petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sf5PetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sf5PetClinicApplication.class, args);
+    }
+}
---
+++ src/test/java/dagimon/spring5course/sf5petclinic/Sf5PetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package dagimon.spring5course.sf5petclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Sf5PetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
