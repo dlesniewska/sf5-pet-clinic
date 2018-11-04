002856743dad241a2bbb6d52de8adf0064b9a99b
dlesniewska <32817937+dlesniewska@users.noreply.github.com>
2018-11-04 23:11:48 +0100

    Add ID to model classes (by extending BaseEntity POJO). Closes #10

--- pet-clinic-data/src/main/java/dagimon/spring5course/sf5petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/dagimon/spring5course/sf5petclinic/model/Person.java
@@ -1,6 +1,6 @@
 package dagimon.spring5course.sf5petclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/dagimon/spring5course/sf5petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/dagimon/spring5course/sf5petclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package dagimon.spring5course.sf5petclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
     private String name;
 
     public String getName() {
