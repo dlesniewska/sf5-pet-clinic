2986e0271625ffb894e49a06d4b92a1fa9767cee
dlesniewska <32817937+dlesniewska@users.noreply.github.com>
2018-11-13 22:37:57 +0100

    Create pettype service Map impl. Closes #29

---
+++ pet-clinic-data/src/main/java/dagimon/spring5course/sf5petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package dagimon.spring5course.sf5petclinic.services;
+
+import dagimon.spring5course.sf5petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/dagimon/spring5course/sf5petclinic/services/map/PetTypeMapServiceImpl.java
@@ -0,0 +1,35 @@
+package dagimon.spring5course.sf5petclinic.services.map;
+
+import dagimon.spring5course.sf5petclinic.model.PetType;
+import dagimon.spring5course.sf5petclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapServiceImpl extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
