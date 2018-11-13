e41b341052f5baff6202bb4827aa3c6583fc360d
dlesniewska <32817937+dlesniewska@users.noreply.github.com>
2018-11-13 22:44:02 +0100

    Add PetTypes in Bootstrap. Closes #32

--- pet-clinic-web/src/main/java/dagimon/spring5course/sf5petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/dagimon/spring5course/sf5petclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package dagimon.spring5course.sf5petclinic.bootstrap;
 
 import dagimon.spring5course.sf5petclinic.model.Owner;
+import dagimon.spring5course.sf5petclinic.model.PetType;
 import dagimon.spring5course.sf5petclinic.model.Vet;
 import dagimon.spring5course.sf5petclinic.services.OwnerService;
+import dagimon.spring5course.sf5petclinic.services.PetTypeService;
 import dagimon.spring5course.sf5petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("dog");
+        PetType savedDogType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("cat");
+        PetType savedCatType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Sylwester");
