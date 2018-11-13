f156115f7db69aafd44b51316675951c26b59ceb
dlesniewska <32817937+dlesniewska@users.noreply.github.com>
2018-11-13 23:10:41 +0100

    Create speciality sevice Map impl. Closes #31

---
+++ pet-clinic-data/src/main/java/dagimon/spring5course/sf5petclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package dagimon.spring5course.sf5petclinic.services;
+
+import dagimon.spring5course.sf5petclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
